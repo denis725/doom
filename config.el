46acb29378859a94147dec7d650415cad01e29f5
Denis <denis725.dev@outlook.com>
2020-12-31 12:56:43 +0100

    Initial doom config

---
+++ config.el
@@ -0,0 +1,253 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Benjamin"
+      user-mail-address "foo@bar.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
+;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq! org-directory "~/work/orga/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq! display-line-numbers-type t)
+
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+
+;; GENERAL
+(setq! evil-want-C-u-scroll nil
+       evil-want-C-d-scroll nil)
+
+;; MacOS modifier keys
+(setq! mac-option-modifier 'super)
+(setq! mac-command-modifier 'meta)
+
+;; when in insert mode, use more or less normal emacs bindings
+(setq! evil-disable-insert-state-bindings t)
+
+;; entering normal mode doesn't move the cursor back
+;; https://www.reddit.com/r/emacs/comments/imrwn7/exiting_insert_mode_places_cursor_1_character_pack/
+(setq! evil-move-cursor-back nil)
+(setq! evil-move-beyond-eol t)
+
+;; faster than SPC u
+(map!
+ :n "U" 'universal-argument)
+
+;; SEARCH
+
+;; avy considers all visible windows
+(setq! avy-all-windows nil)
+
+;; note use g s SPC instead
+(map!
+ :n "g s r" 'counsel-rg)
+
+(map!
+ :nvi "C-s" 'swiper)
+
+;; NAVIGATION
+
+(map!
+ :nv "J" 'forward-paragraph
+ :nv "K" 'backward-paragraph)
+
+(map!
+ :n "ö" 'evil-window-next
+ :n "ä" 'mode-line-other-buffer)
+
+;; a bit easier and more familiar to type than $, existing kbd useless
+(map!
+ :nvim "C-e" 'evil-end-of-line)
+
+;; add move line https://www.emacswiki.org/emacs/MoveLine
+(defun move-line (n)
+  "Move the current line up or down by N lines."
+  (interactive "p")
+  (setq col (current-column))
+  (beginning-of-line) (setq start (point))
+  (end-of-line) (forward-char) (setq end (point))
+  (let ((line-text (delete-and-extract-region start end)))
+    (forward-line n)
+    (insert line-text)
+    ;; restore point to original column in moved line
+    (forward-line -1)
+    (forward-char col)))
+
+(defun move-line-up (n)
+  "Move the current line up by N lines."
+  (interactive "p")
+  (move-line (if (null n) -1 (- n))))
+
+(defun move-line-down (n)
+  "Move the current line down by N lines."
+  (interactive "p")
+  (move-line (if (null n) 1 n)))
+
+(map!
+ :ni "M-<up>" 'move-line-up
+ :ni "M-<down>" 'move-line-down)
+
+;; EDIT
+(map!
+ :nv "M-'" 'comment-region)
+
+;; need this in visual mode, by default M-w (s-w) kills the buffer there
+(map!
+ :nv "M-w" 'kill-ring-save)
+
+
+(defun my-kill-word-or-region-dwim ()
+  "If region active kill it from START to END else backward kill word."
+  ;; Don't use `(interactive "r") (start end)` since that doesn't work
+  ;; when no mark is set (e.g. in a completely new buffer).
+  (interactive)
+  (let ((start (mark)) (end (point)))
+  (if (use-region-p)
+      (kill-region start end)
+    (backward-kill-word 1))))
+
+(map!
+ :i "C-w" #'my-kill-word-or-region-dwim)
+
+;; APPEARANCE
+(add-hook! org-mode 'org-superstar-mode)
+
+(beacon-mode 1)
+
+;; ORG
+
+(map!
+ :mode org-mode
+ "M-<right>" 'org-metaright
+ "M-<left>" 'org-metaleft
+ "M-<up>" 'org-metaup
+ "M-<down>" 'org-metadown)
+
+;; special date and time addition
+(defun my-org-date-time ()
+  "Insert org date and time at the correct position."
+  (interactive)
+  (save-excursion
+    (progn
+      (beginning-of-line)
+       ;; catch error if end of file
+      (condition-case nil (forward-char 3) (error nil))
+      (org-time-stamp 99)
+      (insert " "))))
+
+(defun my-org-date ()
+  "Insert org date at the correct position."
+  (interactive)
+  (save-excursion
+    (progn
+      (beginning-of-line)
+      ;; catch error if end of file
+      (condition-case nil (forward-char 3) (error nil))
+      (org-time-stamp nil)
+      (insert " "))))
+
+
+(map!
+ :mode org-mode
+ :n "SPC m m" 'my-org-date-time)
+
+(map!
+ :mode org-mode
+ :n  "SPC m j" 'my-org-date)
+
+;; capture
+(after! org
+  (add-to-list
+   'org-capture-templates
+     '("s" "screening with rexx"
+      entry (file+headline "rexx.org" "first round")
+      "** TODO %c %^g\n- State \"TODO\"       from              %U")
+     ))
+
+
+;; CODING
+;; python
+(use-package! pyvenv
+  :config (setenv "WORKON_HOME" "~/anaconda3/envs"))
+(map!
+ :mode python-mode
+ "M-<right>" 'python-indent-shift-right
+ "M-<left>" 'python-indent-shift-left)
+
+(map!
+ :mode anaconda-mode
+ :n "M-." 'anaconda-mode-find-definitions
+ :n "M-r" 'anaconda-mode-find-references
+ :n "M-=" 'anaconda-mode-find-assignments)
+
+;; RST (restructured text mode)
+
+(defun my-rst-insert-external-link ()
+  "Insert a link in rst mode."
+  (interactive)
+  (let* ((url (read-string "Enter URL: "))
+         (desc (read-string "Enter description: ")))
+    (insert (format "`%s <%s>`_" desc url))))
+
+(map!
+ :mode rst-mode
+ :ni "C-c C-l" 'my-rst-insert-external-link)
+
+
+;; SHELL
+(defun my-switch-shell-run-last-cmd ()
+  "Switch to shell buffer and run last cmd."
+  (interactive)
+  (let ((bname (buffer-name)))
+    (if (not (string= bname "*shell*"))
+        (other-window 1))
+    (switch-to-buffer "*shell*")
+    (goto-char (point-max))
+    (comint-previous-input 1)
+    (comint-send-input)
+    (if (not (string= bname "*shell*"))
+        (other-window 1))))
+
+(map!
+ :n "SPC o o" 'my-switch-shell-run-last-cmd)
---
+++ custom.el
@@ -0,0 +1,17 @@
+(put 'narrow-to-region 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("bf387180109d222aee6bb089db48ed38403a1e330c9ec69fe1f52460a8936b66" "76bfa9318742342233d8b0b42e824130b3a50dcc732866ff8e47366aed69de11" "99ea831ca79a916f1bd789de366b639d09811501e8c092c85b2cb7d697777f93" "990e24b406787568c592db2b853aa65ecc2dcd08146c0d22293259d400174e37" "1623aa627fecd5877246f48199b8e2856647c99c6acdab506173f9bb8b0a41ac" "f7216d3573e1bd2a2b47a2331f368b45e7b5182ddbe396d02b964b1ea5c5dc27" default))
+ '(package-selected-packages '(window-jump))
+ '(safe-local-variable-values '((flycheck-disabled-checkers quote (python-pylint)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'customize-face 'disabled nil)
